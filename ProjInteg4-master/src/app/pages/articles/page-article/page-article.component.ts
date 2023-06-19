18d6305b4c0bf9a9267c231232370b6eb025a09d
Imadfet <imadfettah7@gmail.com>
2023-06-19 13:30:33 +0100

    imad commit

---
+++ ProjInteg4-master/src/app/pages/articles/page-article/page-article.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-page-article',
+  templateUrl: './page-article.component.html',
+  styleUrls: ['./page-article.component.scss']
+})
+export class PageArticleComponent {
+
+
+  constructor(
+    private router: Router
+  ) { }
+  nouvelArticle(): void {
+    this.router.navigate(['nouvelarticle']);
+  }
+}
